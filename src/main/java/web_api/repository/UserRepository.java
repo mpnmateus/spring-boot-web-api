7ade3e0a6c2c843a8dfffa7795459a5b9fcfbb39
mateusnovo <mpnmateus@gmail.com>
2024-10-10 19:59:49 -0300

    feat: implementando metodo findByAll para usar em webservice Controller

--- src/main/java/web_api/repository/UserRepository.java
+++ src/main/java/web_api/repository/UserRepository.java
@@ -3,6 +3,9 @@ package web_api.repository;
 import org.springframework.stereotype.Repository;
 import web_api.model.Usuario;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class UserRepository {
     //fake repository
@@ -16,5 +19,12 @@ public class UserRepository {
     public void deleteById(Integer id){
         System.out.println(String.format("DELETE/id - Recebendo o id %d para excluir um usuário.", id));
         System.out.println(id);
+    }
+    public List<Usuario> findByAll(){
+        System.out.println("LIST - Listando os usuários do sistema.");
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("mateusp", "123456"));
+        usuarios.add(new Usuario("Carla", "123456"));
+        return usuarios;
     }
 }
