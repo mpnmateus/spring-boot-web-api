15122399e5d3029af00b66519a338441c6fb966f
mateusnovo <mpnmateus@gmail.com>
2024-10-10 19:54:00 -0300

    feat: implementando metodo deleteById para usar no controller

--- src/main/java/web_api/repository/UserRepository.java
+++ src/main/java/web_api/repository/UserRepository.java
@@ -5,11 +5,16 @@ import web_api.model.Usuario;
 
 @Repository
 public class UserRepository {
+    //fake repository
     public void save(Usuario usuario){
         if(usuario.getId() == null)
             System.out.println("SAVE - Recebendo o usuário na camada de repositório.");
         else
             System.out.println("UPDATE - Recebendo o usuário na camada de repositório.");
         System.out.println(usuario);
+    }
+    public void deleteById(Integer id){
+        System.out.println(String.format("DELETE/id - Recebendo o id %d para excluir um usuário.", id));
+        System.out.println(id);
     }
 }
