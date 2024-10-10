045e4f4110ea8e6866f1635f090bec4e0f2d7257
mateusnovo <mpnmateus@gmail.com>
2024-10-10 19:19:44 -0300

    feat: tornando a classe welcome em um webservice, com recurso GET

--- src/main/java/web_api/controller/WelcomeController.java
+++ src/main/java/web_api/controller/WelcomeController.java
@@ -1,6 +1,11 @@
 package web_api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+    @GetMapping
     public String welcome(){
         return "welcome to My Spring Boot Web API";
     }
