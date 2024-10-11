02faa04e5f7ccfbb787c075a9056e956b2ca8bdd
mateusnovo <mpnmateus@gmail.com>
2024-10-10 21:44:59 -0300

    refactor: anotacao que inclui prefixo users em todos os mapeamentos desse controller

--- src/main/java/web_api/controller/UsuarioController.java
+++ src/main/java/web_api/controller/UsuarioController.java
@@ -8,22 +8,23 @@ import web_api.repository.UsuarioRepository;
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
-    @GetMapping("/users")
+    @GetMapping()
     public List<Usuario> getUsers(){
         return repository.findByAll();
     }
-    @GetMapping("/users/{username}")
+    @GetMapping("/{username}")
     public Usuario getOne(@PathVariable("username") String username){
         return repository.findByUsername(username);
     }
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/{id}")
     public void deleteUser (@PathVariable("id") Integer id){
         repository.deleteById(id);
     }
-    @PostMapping("/users")
+    @PostMapping()
     public void postUser(@RequestBody Usuario usuario){
         repository.save(usuario);
     }
