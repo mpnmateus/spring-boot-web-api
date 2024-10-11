f6f0065102bcbf887cab43836f278fac68c3f20a
mateusnovo <mpnmateus@gmail.com>
2024-10-11 16:06:05 -0300

    feat: cinfigurando swagger, classe Contact

--- src/main/java/web_api/doc/SwaggerConfig.java
+++ src/main/java/web_api/doc/SwaggerConfig.java
@@ -1,10 +1,19 @@
 package web_api.doc;
 
+
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.Contact;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    private Contact contato (){
+        return new Contact(
+                "Mateus",
+                "www.mateus.ai",
+                "mateus111@gmail.com"
+        );
+    }
 }
