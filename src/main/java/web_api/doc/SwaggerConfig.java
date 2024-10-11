6f33dcbcdac983e78c9ac82f071c6a60e51c5d32
mateusnovo <mpnmateus@gmail.com>
2024-10-11 17:32:48 -0300

    refactor: refatorando classe de configuracao swagger para ficar compativel com nova dependencia, usando outros pacotes e classes

--- src/main/java/web_api/doc/SwaggerConfig.java
+++ src/main/java/web_api/doc/SwaggerConfig.java
@@ -1,19 +1,40 @@
 package web_api.doc;
 
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.service.Contact;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
-    private Contact contato (){
-        return new Contact(
-                "Mateus",
-                "www.mateus.ai",
-                "mateus111@gmail.com"
-        );
+
+    // Método para configurar as informações de contato
+    private Contact contato() {
+        return new Contact()
+                .name("Mateus")
+                .url("www.mateus.ai")
+                .email("mateus111@gmail.com");
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Title - Rest API")
+                        .description("API exemplo de uso de Springboot REST API")
+                        .version("1.0")
+                        .termsOfService("Termo de uso: Open Source")
+                        .license(new License()
+                                .name("Licença - Sua Empresa")
+                                .url("http://www.seusite.com.br"))
+                        .contact(contato()))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Documentação Externa")
+                        .url("https://github.com/mateus111"));
     }
 }
