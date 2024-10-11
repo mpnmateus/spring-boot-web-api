27d28c196062de28e994469280c4bfca4cae297c
mateusnovo <mpnmateus@gmail.com>
2024-10-10 21:40:38 -0300

    feat: metodo post mapeado com conversao de JSON para estrutura do objeto, delegando responsa para repository

--- src/main/java/web_api/controller/UsuarioController.java
+++ src/main/java/web_api/controller/UsuarioController.java
@@ -1,9 +1,7 @@
 package web_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import web_api.model.Usuario;
 import web_api.repository.UsuarioRepository;
 
@@ -20,5 +18,13 @@ public class UsuarioController {
     @GetMapping("/users/{username}")
     public Usuario getOne(@PathVariable("username") String username){
         return repository.findByUsername(username);
+    }
+    @DeleteMapping("/users/{id}")
+    public void deleteUser (@PathVariable("id") Integer id){
+        repository.deleteById(id);
+    }
+    @PostMapping("/users")
+    public void postUser(@RequestBody Usuario usuario){
+        repository.save(usuario);
     }
 }
