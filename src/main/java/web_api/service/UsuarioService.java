c4384dc1cc342dc3c653ba01a723182b72f500a0
mateusnovo <mpnmateus@gmail.com>
2024-10-11 13:46:28 -0300

    refactor: adicionando mais uma camada de service, que ficara responsavel pela logica de negocios

---
+++ src/main/java/web_api/service/UsuarioService.java
@@ -0,0 +1,9 @@
+package web_api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+    //responsável pela lógica de negócio e comunicação com a camada de repositório.
+    //implementar futuramente, modificando as outras camadas
+}
